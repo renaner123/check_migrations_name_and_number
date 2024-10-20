6bde911382493c482ab15004584a81d63f0eba2e
renaner123 <renaner123@gmail.com>
2024-10-20 16:56:46 -0300

    altera impressões no console

--- scripts/check_migrations.sh
+++ scripts/check_migrations.sh
@@ -19,16 +19,12 @@ do
     numeros_main=$(echo "$arquivos_main" | sed -E 's/^([0-9]+)-.*/\1/' | sort -u)
     numeros_atual=$(echo "$arquivos_atual" | sed -E 's/^([0-9]+)-.*/\1/' | sort -u)
 
-    echo -e "Números main:\n$numeros_main"
-    echo -e "Números atual:\n$numeros_atual"
-
     # Verificar duplicatas apenas nos novos arquivos
     duplicados=$(echo "$numeros_atual" | grep -Fxf <(echo "$numeros_main"))
 
     if [ -n "$duplicados" ]; then
         echo "Erro: Arquivos com numeração duplicada encontrados na pasta $DIR!"
-        echo "Números duplicados: $duplicados"
-
+     
         # Listar apenas os novos arquivos que causaram a duplicação
         for numero in $duplicados; do
             arquivos_duplicados=$(echo "$arquivos_atual" | grep "^$numero-" | grep -v -F -x -f <(echo "$arquivos_main"))
