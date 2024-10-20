a62dd75f088dc7bcf9b59417d0bc355f54d95a5c
renaner123 <renaner123@gmail.com>
2024-10-20 16:43:33 -0300

    altera lógica de comparação migration

--- scripts/check_migrations.sh
+++ scripts/check_migrations.sh
@@ -2,38 +2,37 @@
 
 SQL_DIRS=("./migrations/oracle" "./migrations/mssql" "./migrations/postgres")
 
-# Verifica os arquivos presentes na branch main
 git fetch origin main
 
 for DIR in "${SQL_DIRS[@]}"
 do
-    echo "Verificando duplicidade na pasta: $DIR"
-  
-    # Verifica os arquivos presentes na branch main
+    echo -e "\nVerificando duplicidade na pasta: $DIR"
+
+    # Listar apenas os nomes dos arquivos sem o caminho
     arquivos_main=$(git ls-tree -r --name-only origin/main $DIR | sed 's|.*/||' | grep -E '^[0-9]+-.*\.sql$')
-
-    echo -e "Arquivos na branch main:\n$arquivos_main"
-
-    # Verifica os arquivos presentes na branch atual
     arquivos_atual=$(git ls-tree -r --name-only HEAD $DIR | sed 's|.*/||' | grep -E '^[0-9]+-.*\.sql$')
 
-    echo -e "Arquivos na branch atual:\n$arquivos_atual \n"
+    echo -e "Arquivos na branch main:\n$arquivos_main"
+    echo -e "Arquivos na branch atual:\n$arquivos_atual"
 
-    # Extrai os prefixos numéricos dos arquivos
+    # Extrair os números diretamente dos nomes dos arquivos
     numeros_main=$(echo "$arquivos_main" | sed -E 's/^([0-9]+)-.*/\1/' | sort -u)
     numeros_atual=$(echo "$arquivos_atual" | sed -E 's/^([0-9]+)-.*/\1/' | sort -u)
 
-    # Verifica se há duplicatas
+    echo -e "Números main:\n$numeros_main"
+    echo -e "Números atual:\n$numeros_atual"
+
+    # Verificar duplicatas apenas nos novos arquivos
     duplicados=$(echo "$numeros_atual" | grep -Fxf <(echo "$numeros_main"))
 
     if [ -n "$duplicados" ]; then
         echo "Erro: Arquivos com numeração duplicada encontrados na pasta $DIR!"
         echo "Números duplicados: $duplicados"
 
-        # Listar os arquivos que causaram a duplicação
+        # Listar apenas os novos arquivos que causaram a duplicação
         for numero in $duplicados; do
             echo "Arquivos duplicados para o número $numero:"
-            echo "$arquivos_atual" | grep "^$numero-"
+            echo "$arquivos_atual" | grep "^$numero-" | grep -v -F -x -f <(echo "$arquivos_main")
         done
 
         exit 1
