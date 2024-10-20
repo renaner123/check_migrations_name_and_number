cdf065ca9d84931e48fe2491ade13fdaadf73791
renaner123 <renaner123@gmail.com>
2024-10-20 15:50:53 -0300

    correção nome branch main

--- scripts/check_migrations.sh
+++ scripts/check_migrations.sh
@@ -2,25 +2,25 @@
 
 SQL_DIRS=("migrations/oracle" "migrations/mssql" "migrations/postgres")
 
-# Verifica os arquivos presentes na branch master
-git fetch origin master
+# Verifica os arquivos presentes na branch main
+git fetch origin main
 
 for DIR in "${SQL_DIRS[@]}"
 do
     echo "Verificando duplicidade na pasta: $DIR"
 
-    # Verifica os arquivos presentes na branch master
-    arquivos_master=$(git ls-tree -r --name-only origin/master $DIR | grep -E '^[0-9]+-.*\.sql$')
+    # Verifica os arquivos presentes na branch main
+    arquivos_main=$(git ls-tree -r --name-only origin/main $DIR | grep -E '^[0-9]+-.*\.sql$')
 
     # Verifica os arquivos presentes na branch atual
     arquivos_atual=$(git ls-tree -r --name-only HEAD $DIR | grep -E '^[0-9]+-.*\.sql$')
 
     # Extrai os prefixos numéricos dos arquivos
-    numeros_master=$(echo "$arquivos_master" | grep -oE '^[0-9]+')
+    numeros_main=$(echo "$arquivos_main" | grep -oE '^[0-9]+')
     numeros_atual=$(echo "$arquivos_atual" | grep -oE '^[0-9]+')
 
     # Verifica se há duplicatas
-    duplicados=$(echo "$numeros_atual" | grep -Fxf <(echo "$numeros_master"))
+    duplicados=$(echo "$numeros_atual" | grep -Fxf <(echo "$numeros_main"))
 
     if [ -n "$duplicados" ]; then
         echo "Erro: Arquivos com numeração duplicada encontrados na pasta $DIR!"
